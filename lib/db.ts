f3b8979de84fd85f8021354a6231552154f6c5ee
aadesh-cisit <aadesh.kumar@cisit.in>
2025-06-27 13:17:15 +0530

    DB NAME CHANGE

--- lib/db.ts
+++ lib/db.ts
@@ -13,7 +13,7 @@ export const connectToDB = async () => {
 
   try {
     await mongoose.connect(process.env.MONGODB_URI, {
-      dbName: "fesensi-community",
+      dbName: "Fesensi-community",
     });
 
     isConnected = true;
