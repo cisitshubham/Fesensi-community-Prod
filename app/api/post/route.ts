bbae74c2854f5f9f41b31248d810d2685e2d6993
aadesh-kumarr <116622502+aadesh-kumarr@users.noreply.github.com>
2025-08-21 04:34:54 +0530

    feat: add API endpoint to fetch posts with filtering options

---
+++ app/api/post/route.ts
@@ -0,0 +1,50 @@
+"use server";
+
+
+import { connectToDB } from "@/lib/db";
+import Post from "@/models/post.model";
+import User from "@/models/user.model";
+import Comment from "@/models/comment.model";
+import { NextResponse } from "next/server";
+
+
+
+
+
+export async function GET(req: Request) {
+  try {
+    await connectToDB();
+
+    // Parse filter from search params
+    const { searchParams } = new URL(req.url);
+    const filter = searchParams.get("filter");
+
+    let sortOptions = {};
+    let query = {};
+    switch (filter) {
+      case "newest":
+        sortOptions = { createdAt: -1 };
+        break;
+      case "most_viewed":
+        sortOptions = { views: -1 };
+        break;
+      case "uncommented":
+        query = { comments: { $size: 0 } };
+        break;
+      default:
+        break;
+    }
+
+  query = { ...query, isApproved: true };
+  const posts = await Post.find(query);
+
+  const response = NextResponse.json(posts);
+  response.headers.set("Access-Control-Allow-Origin", "*");
+  response.headers.set("Access-Control-Allow-Methods", "GET, OPTIONS");
+  response.headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  return response;
+  } catch (error) {
+    console.log("[FETCH ALL_POSTS ERROR]", error);
+    return NextResponse.json({ error: "Failed to fetch posts" }, { status: 500 });
+  }
+}
